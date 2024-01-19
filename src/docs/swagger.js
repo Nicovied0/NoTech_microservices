79d4804691ff29adf2844acedea1c585407877f7
Nicovied0 <briannoviedo@gmail.com>
2024-01-19 14:59:55 -0300

    asdadk

--- src/docs/swagger.js
+++ src/docs/swagger.js
@@ -18,19 +18,23 @@ const options = {
 const swaggerSpec = swaggerJSDoc(options);
 
 const configureSwagger = (app, port) => {
-  // Serve Swagger UI assets
-  app.use("/api/docs", swaggerUi.serve);
-
-  // Serve Swagger JSON spec
+  app.use(
+    "/api/docs",
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerSpec, {
+      swaggerOptions: {
+        url: "/api/docs.json",
+        urls: [{ url: "/api/docs/swagger-ui-bundle.js", name: "swagger-ui-bundle.js" }],
+      },
+    })
+  );
   app.get("/api/docs.json", (req, res) => {
     res.setHeader("Content-Type", "application/json");
     res.send(swaggerSpec);
   });
-
-  // Swagger UI setup
-  app.get("/api/docs", swaggerUi.setup(swaggerSpec, { explorer: true }));
-
-  console.log(`Swagger documentation available at http://localhost:${port}/api/docs`);
+  console.log(
+    `Swagger documentation available at http://localhost:${port}/api/docs`
+  );
 };
 
 module.exports = { configureSwagger };
